b3ed270f3fd153266ed482428c6fa707eecba830
BestForTravel <1bestfortravel@gmail.com>
2025-10-12 13:03:56 -0400

    Header updated

--- ReactJS/src/Layout.js
+++ ReactJS/src/Layout.js
@@ -1,10 +1,18 @@
-import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
+import Header from './components/Header';
+import { Outlet, useLocation } from 'react-router-dom';
 
 function Layout() {
+  const location = useLocation();
+
+  // define which routes should have full-width header
+  const fullWidthRoutes = ['/map', '/albums']
+
+  // check if the current route matches one of them
+  const isFullWidth = fullWidthRoutes.includes(location.pathname)
+
   return (
     <>
-      <Header />
+      <Header fullWidth={isFullWidth} />
       <Outlet />
     </>
   );
--- ReactJS/src/components/Header/Header.jsx
+++ ReactJS/src/components/Header/Header.jsx
@@ -1,42 +1,32 @@
-import "./Header.scss";
-import React from "react";
-import { Link } from 'react-router-dom';
+import './Header.scss'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-function template() {
+export default function Header({ fullWidth = false }) {
   return (
-    <React.Fragment>
-        <div className="wrapper header-wrapper">
-          <header>
-            <div className="header-inner">
-              <div className="logo-wrapper">
-                <Link className="header-link" to='/feed'>
-                  <img src="./icons/LogoFull.svg" alt="Feed"/>
-                </Link>
-              </div>
-              <div className="header-links-wrapper">
-                <Link className="header-link" to='/notFound'>
-                  Map
-                </Link>
-                <Link className="header-link" to='/feed'>
-                  Posts
-                </Link>
-                <Link className="header-link" to='#'>
-                  Next Trip
-                </Link>
-                <Link className="header-link" to='#'>
-                  Notes
-                </Link>
-              </div>
-              <div className="avatar-wrapper">
-                <Link className="profile" to='/profile'>
-                  <img className="avatar-small" src="./images/avatar.png" alt="Home"/>
-                </Link>
-              </div>
-            </div>
-          </header>
-        </div>
-    </React.Fragment>
-  );
-};
+    <div className={`${fullWidth ? 'wrapper-full-width' : 'wrapper'} header-wrapper`}>
+      <header>
+        <div className='header-inner'>
+          <div className='logo-wrapper'>
+            <Link className='header-link' to='/feed'>
+              <img src='./icons/LogoFull.svg' alt='Feed' />
+            </Link>
+          </div>
 
-export default template;
+          <div className='header-links-wrapper'>
+            <Link className='header-link' to='/map'>Map</Link>
+            <Link className='header-link' to='/feed'>Posts</Link>
+            <Link className='header-link' to='#'>Next Trip</Link>
+            <Link className='header-link' to='#'>Notes</Link>
+          </div>
+
+          <div className='avatar-wrapper'>
+            <Link className='profile' to='/profile'>
+              <img className='avatar-small' src='./images/avatar.png' alt='Home' />
+            </Link>
+          </div>
+        </div>
+      </header>
+    </div>
+  )
+}
